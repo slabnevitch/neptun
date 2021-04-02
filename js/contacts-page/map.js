0852c6bd50a33b7a4cb5c780b1a3b079e58acfea
slabnevitch <handofschpilrain@mail.ru>
2021-04-02 10:14:04 +0500

    contacts page deploy

---
+++ js/contacts-page/map.js
@@ -0,0 +1,27 @@
+// yamaps
+		ymaps.ready(init);
+		function init(){
+			var myMap = new ymaps.Map("contacts-map", {
+				center: [53.195042, 45.018316],
+				zoom: 17
+       	// controls: ['zoomControl'], //оставляем только масштабирование
+       	// behaviors: ['drag'] //оставляем только поведение drag
+      });
+
+		  // одиночный маркер 
+	      //   var placemark = new ymaps.Placemark(myMap.getCenter(), {
+	      //   		hintContent: null,
+	      //       balloonContent: null
+	      //   	},
+	      //   	{
+	      //   		// стилизация собственной метки
+	      //   	 iconLayout: 'default#image',
+						 // iconImageHref: 'path-to-image',
+						 //  iconImageSize: [50, 50],
+						 //   iconImageOffset: [-25, -50]
+
+	      // 	});
+
+	        // myMap.geoObjects.add(placemark);
+	}
+// END yamaps
